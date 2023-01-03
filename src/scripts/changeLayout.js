64ab260c451437cc1f3b915bd68152b7bd9f214d
vjendita-pllana <vjenditapllana@hotmail.com>
2023-01-03 02:32:34 +0100

    add icons+onhover text, change uni/waterfall icon

--- src/scripts/changeLayout.js
+++ src/scripts/changeLayout.js
@@ -1,9 +1,22 @@
 const changeLayoutBtn = document.getElementById("change-layout-btn");
 const container = document.getElementById("posts-container");
+const waterfallBtn = document.querySelector(".waterfall");
+const uniformBtn = document.querySelector(".uniform");
 
 const changeLayout = () => {
   container.classList.toggle("dense-grid");
   container.classList.toggle("flex-container");
 };
 
+const changeButton = () => {
+  if (waterfallBtn.style.display != "flex") {
+      waterfallBtn.style.display = "flex"
+      uniformBtn.style.display = "none"
+  } else {
+    waterfallBtn.style.display = "none"
+    uniformBtn.style.display = "flex"
+    }
+}
+
 changeLayoutBtn.addEventListener("click", changeLayout);
+changeLayoutBtn.addEventListener("click", changeButton);
