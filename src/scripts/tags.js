466c2031c83e94635c94941bb09cf637549095f8
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-01-04 13:48:06 +0100

    Post tags

---
+++ src/scripts/tags.js
@@ -0,0 +1,15 @@
+export function singleTag(
+    id,
+    name,
+    display_name,
+    followers,
+    total_items,
+    background_hash
+){
+    let tag = `<div class="tag 
+    text-white 
+    rounded-sm 
+    overflow-hidden 
+    "  id="image-container" 
+    data-id=${id}>`;
+}
