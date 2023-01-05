b278d12fbc5292d979aa2629f211b7e5f23073fc
vjendita-pllana <vjenditapllana@hotmail.com>
2023-01-05 18:14:42 +0100

    add modal input elements and customize their style

--- src/main.js
+++ src/main.js
@@ -2,3 +2,28 @@ import { addGalleryImages } from "./scripts/addGalleryImages.js";
 import changeLayout from "./scripts/changeLayout.js";
 
 addGalleryImages("https://api.npoint.io/bc13239283496e6574a7");
+const input = document.querySelector("#paste");
+console.log(input.value)
+
+input.addEventListener('focus', () => {
+    input.value = "";
+})
+input.addEventListener('blur', () => {
+    input.value = "Paste image or URL";
+})
+
+
+const inputFile = document.querySelector('#file-input');
+
+inputFile.addEventListener('change', () => {
+    inputFile.files[0];
+});
+
+const dropInput = document.querySelector('#drop-input');
+
+inputFile.addEventListener('change', () => {
+    inputFile.files[0];
+});
+
+
+
