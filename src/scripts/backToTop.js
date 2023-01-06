b33d6d0c399bf75f311790c2f2a893cea70928ad
Valtrim <valtrim01@gmail.com>
2023-01-06 13:28:05 +0100

    User

--- src/scripts/backToTop.js
+++ src/scripts/backToTop.js
@@ -19,3 +19,21 @@ window.addEventListener('scroll', function() {
       }
     }, 10);
   });
+
+
+//////////// route //////////////
+
+function toggle(IDS) {
+  var sel = document.getElementById('pg').getElementsByClassName('pg');
+  var a = document.getElementById('pages-container').getElementsByTagName('a');
+  for (var i=0; i<sel.length; i++) { 
+    if (sel[i].id != IDS) { sel[i].style.display = 'none'; }
+    if (sel[i].id == IDS) { sel[i].style.display = 'block'; }
+  }
+
+  for (var i=0; i<a.length; i++) { 
+    if (a[i].id != IDS) { a[i].classList.remove('active-page');}
+    if (a[i].id == IDS) { a[i].classList.add('active-page');}
+  }
+  return false;
+}
