106ff21d67db956901eb18f7be7b13d95be5f26b
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-01-18 18:15:11 +0100

    print Username and password in console on Sign In page

---
+++ src/scripts/login.js
@@ -0,0 +1,10 @@
+document.getElementById("sign-in").addEventListener("click", function userInfo() {
+
+    var emai = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    
+    
+
+    console.log(`UserName: ${emai}`);
+    console.log(`Password: ${pass}`);
+})
