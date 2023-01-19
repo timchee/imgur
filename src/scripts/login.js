0bca0d41ed767577ba0d1a68ecb89d9cdffeb9cc
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-01-19 23:57:21 +0100

    sign in and sign up responsive and js fixes

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,10 +1,10 @@
 document.getElementById("sign-in").addEventListener("click", function userInfo() {
 
-    var emai = document.getElementById("email").value;
+    var Email = document.getElementById("email").value;
     var pass = document.getElementById("password").value;
     
     
 
-    console.log(`UserName: ${emai}`);
+    console.log(`UserName: ${Emai}`);
     console.log(`Password: ${pass}`);
 })
