1d0544f3006bbb3d4eb19edf458a426ff7a20ad3
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-02-02 23:27:21 +0100

    session storage and username alert

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,18 +1,25 @@
-
-
-sessionStorage.setItem("loggedIn", false)
+sessionStorage.setItem("loggedIn", false);
 const login = () => {
   sessionStorage.setItem("loggedIn", true);
   const email = document.getElementById("email").value;
+  const psw = document.getElementById("password").value.length;
+  var wrongPsw = document.getElementById("hidden");
+  var usernam = document.getElementById("username");
+
+  let user = "user";
   let username = email.split("@")[0];
-  if (username === '') {
-    username = 'User'
+  if (username.length == 0) {
+    usernam.classList.remove("hidden");
+  } else if (username == user && psw >= 8) {
+    sessionStorage.setItem("username", username);
+    window.history.back();
+  } else {
+    wrongPsw.classList.remove("hidden");
   }
-  sessionStorage.setItem("username", username);
-  window.history.back();
-  
+
+  if (username.length >= 1) {
+    usernam.classList.add("hidden");
+  }
 };
 
 document.getElementById("sign-in").addEventListener("click", login);
-
-
