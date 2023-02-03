1cdda32a1a8e16e70d876832e63029fbeefbb4aa
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-02-03 17:55:34 +0100

    regex fix

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,16 +1,14 @@
+
+
 sessionStorage.setItem("loggedIn", false);
 const login = () => {
   sessionStorage.setItem("loggedIn", true);
   const email = document.getElementById("email").value;
   const psw = document.getElementById("password").value.length;
   var wrongPsw = document.getElementById("hidden");
-  var usernam = document.getElementById("usernamee");
 
-  let user = "user";
   let username = email.split("@")[0];
-  if (username.length == 0) {
-    usernam.classList.remove("hidden");
-  } else if (username == user && psw >= 8) {
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && psw >= 8) {
     sessionStorage.setItem("username", username);
     window.location.href = "../";
 
